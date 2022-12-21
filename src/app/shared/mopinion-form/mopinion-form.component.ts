c1f7ef0fa9138f731bd459e898c9e15a31f4adbc
Chin-On, Ziran (AV) <ZChinOn@svb.nl>
2022-12-21 18:59:06 +0100

    Loading mopinion after route change now works, due to the addition of a timeout and a global window variable which is used as a loading condition. The loading condition is needed because there is no way of manually clearing the memory in the Mopinion javascript. We can force this by changing the condition, in this case we toggle the condition onInit and onDestroy.

--- src/app/shared/mopinion-form/mopinion-form.component.ts
+++ src/app/shared/mopinion-form/mopinion-form.component.ts
@@ -1,22 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-mopinion-form',
   templateUrl: './mopinion-form.component.html',
   styleUrls: ['./mopinion-form.component.scss']
 })
-export class MopinionFormComponent implements OnInit {
+export class MopinionFormComponent implements OnInit, OnDestroy {
 
   constructor() { }
 
   ngOnInit(): void {
-    // Call forceParse
-    try {
-      // @ts-ignore
-      Pastease.forceParse();
-    }catch(err){
-      console.warn('Pastease could not parse. Please check if Pastease was loaded on the page.')
-    }
+    setTimeout(() => {
+      (<any> window).showForm = true;
+
+      // Call forceParse
+      try {
+        // @ts-ignore
+        Pastease.forceParse();
+      } catch (err) {
+        console.warn('Pastease could not parse. Please check if Pastease was loaded on the page.')
+      }
+    }, 1000);
   }
 
+  ngOnDestroy(): void {
+    (<any>window).showForm = false;
+  }
 }
